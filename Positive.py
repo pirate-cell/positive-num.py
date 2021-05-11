dd359bdeb13add537852e0b75171b72372da0b83
pirate-cell <84022705+pirate-cell@users.noreply.github.com>
2021-05-11 19:28:29 +0530

    Print.py
    Positive numbers on list will print on screen

---
+++ Positive.py
@@ -0,0 +1,11 @@
+# positive-num.py
+Print positive Numbers on list
+# list of numbers
+list 1 = [12,-7,5,64,-14]
+list 2 = [12,14,-95,3]
+for num in list 1:
+for num in list 2:    
+
+     if num >= 0:
+       print(num, end = " ")
+print("positive number on list")
